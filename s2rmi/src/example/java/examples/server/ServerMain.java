bb3b20e1b076be9001ba52d210cb6e7210fef113
Koichi Kobayashi <koichik@improvement.jp>
2006-07-28 14:11:35 +0000

    [JIRA] RMI-1
    git-svn-id: https://www.seasar.org/svn/s2rmi/trunk@26 53528cc7-d406-0410-b48a-a79fbf270018

--- s2rmi/src/example/java/examples/server/ServerMain.java
+++ s2rmi/src/example/java/examples/server/ServerMain.java
@@ -1,14 +1,19 @@
 package examples.server;
 
+import org.seasar.framework.container.external.GenericS2ContainerInitializer;
 import org.seasar.framework.container.factory.S2ContainerFactory;
 
 /**
  * @author Kenichiro Murata
+ * @author koichik
  */
 public class ServerMain {
 
     public static void main(String[] args) {
-        S2ContainerFactory.create("examples/server/server.dicon").init();
+        S2ContainerFactory.configure("examples/server/s2container.dicon");
+        GenericS2ContainerInitializer initializer = new GenericS2ContainerInitializer();
+        initializer.setConfigPath("examples/server/server.dicon");
+        initializer.initialize();
     }
 
 }
